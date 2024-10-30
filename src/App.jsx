1e1daefc63a9247c2ac2e245e2e1bb38912e5bd8
mucahitcetin <mucahitcetin01@gmail.com>
2024-10-30 22:39:11 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
+};
+
+export default App;
